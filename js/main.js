09a8630f64b0976fe6244e0aeb2e4ce27b306dee
Mark Cahill <mark@thinkjson.com>
2014-07-29 20:28:14 -0400

    Reskinned, added localStorage support

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var app = angular.module('color', []);
 var MULTIPLIER = 50;
 
 app.controller('MainController', ['$scope', function($scope) {
-	$scope.swatches = [
+	$scope.swatches = localStorage.swatches ? JSON.parse(localStorage.swatches) : [
 		[255,0,0],
 		[0,255,0],
 		[0,0,255],
@@ -17,17 +17,21 @@ app.controller('MainController', ['$scope', function($scope) {
 		$scope.red = Math.floor(255 * Math.random());
 		$scope.green = Math.floor(255 * Math.random());
 		$scope.blue = Math.floor(255 * Math.random());
+
+		localStorage.swatches = JSON.stringify($scope.swatches);
 	}
 
 	$scope.rgb2hex = function(red, green, blue) {
 		var rgb = blue | (green << 8) | (red << 16);
-    	return '#' + rgb.toString(16);
+    	return '#' + ("000000" + rgb.toString(16)).substr(-6);
 	}
 
 	$scope.mix = function(red, green, blue) {
 		$scope.red += Math.ceil((red-$scope.red)/MULTIPLIER);
 		$scope.green += Math.ceil((green-$scope.green)/MULTIPLIER);
 		$scope.blue += Math.ceil((blue-$scope.blue)/MULTIPLIER);
+
+		localStorage.swatches = JSON.stringify($scope.swatches);
 	};
 
 	$scope.save = function() {
@@ -36,5 +40,7 @@ app.controller('MainController', ['$scope', function($scope) {
 			$scope.green,
 			$scope.blue
 		]);
+
+		localStorage.swatches = JSON.stringify($scope.swatches);
 	}
 }]);
